f23417c751f516e628d7729889d6b41baebe6df1
Eugeniu Tufar <pacific.trac@gmail.com>
2025-02-06 17:03:33 +0100

    Add ActiveWordPreference

---
+++ app/src/main/java/com/link184/oneword/data/ActiveWordPreference.kt
@@ -0,0 +1,20 @@
+package com.link184.oneword.data
+
+import android.content.Context
+import androidx.core.content.edit
+
+interface ActiveWordPreference {
+    var activeWordId: Long
+}
+
+internal const val ACTIVE_WORD_SHARED_PREFERENCE_NAME = "active_word_preferences"
+internal const val ACTIVE_WORD_ID_PREFERENCE_KEY = "active_word_id"
+
+internal class DefaultActiveWordsPreference(context: Context): ActiveWordPreference {
+    private val activeWordShaderPreference = context.getSharedPreferences(
+        ACTIVE_WORD_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+
+    override var activeWordId: Long
+        get() = activeWordShaderPreference.getLong(ACTIVE_WORD_ID_PREFERENCE_KEY, -1)
+        set(value) = activeWordShaderPreference.edit { putLong(ACTIVE_WORD_ID_PREFERENCE_KEY, value) }
+}
---
+++ app/src/test/java/com/link184/oneword/data/DefaultActiveWordsPreferenceTest.kt
@@ -0,0 +1,43 @@
+package com.link184.oneword.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class DefaultActiveWordsPreferenceTest {
+    private lateinit var classUnderTest : DefaultActiveWordsPreference
+    @MockK(relaxed = true)
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        val context: Context = mockk()
+        every { context.getSharedPreferences(ACTIVE_WORD_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE) } returns sharedPreferences
+        classUnderTest = DefaultActiveWordsPreference(context)
+    }
+
+    @Test
+    fun `Given word id When set and get it Then the result must match`() {
+        // Given
+        val wordId = 666L
+        every { sharedPreferences.getLong(ACTIVE_WORD_ID_PREFERENCE_KEY, -1) } returns wordId
+
+        // When
+        classUnderTest.activeWordId = wordId
+        val actualResult = classUnderTest.activeWordId
+
+        // Then
+        assertEquals(wordId, actualResult)
+    }
+}
